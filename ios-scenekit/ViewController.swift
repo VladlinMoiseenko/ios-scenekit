fe13bfa835c0eeccc00301841b0133be313f94e4
mvladlin@gmail.com <mvladlin@gmail.com>
2019-04-05 22:22:21 +0300

    update

--- ios-scenekit/ViewController.swift
+++ ios-scenekit/ViewController.swift
@@ -5,21 +5,34 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var sceneView: ARSCNView!
     
-    let comfiguration = ARWorldTrackingConfiguration()
+    let configuration = ARWorldTrackingConfiguration()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
-        self.sceneView.session.run(comfiguration)
+        self.sceneView.session.run(configuration)
     }
 
     @IBAction func add(_ sender: Any) {
         let node = SCNNode()
         node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.1)
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.green
+        node.position = SCNVector3(0,0,-0.5)
         self.sceneView.scene.rootNode.addChildNode(node)
         
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        restartSession()
+    }
+    
+    func restartSession() {
+        self.sceneView.session.pause()
+        self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+            node.removeFromParentNode()
+        }
+        self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
     
 }
