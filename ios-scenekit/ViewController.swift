e3a1908d7cf8a4b0a1797755a250a2d1248ae37d
mvladlin@gmail.com <mvladlin@gmail.com>
2019-04-06 09:50:19 +0300

    update

--- ios-scenekit/ViewController.swift
+++ ios-scenekit/ViewController.swift
@@ -12,14 +12,51 @@ class ViewController: UIViewController {
         
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         self.sceneView.session.run(configuration)
+        //self.sceneView.autoenablesDefaultLighting = true
     }
 
     @IBAction func add(_ sender: Any) {
-        let node = SCNNode()
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.1)
-        node.geometry?.firstMaterial?.diffuse.contents = UIColor.green
-        node.position = SCNVector3(0,0,-0.5)
-        self.sceneView.scene.rootNode.addChildNode(node)
+        
+        // 1 variant
+//        let node = SCNNode()
+//        node.geometry = SCNText(string: "Hello", extrusionDepth: 1)
+//        //node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
+//        node.geometry?.firstMaterial?.diffuse.contents = UIColor.purple
+//        //node.localRotate(by: SCNQuaternion()
+//        node.position = SCNVector3(0,0,0)
+//        self.sceneView.scene.rootNode.addChildNode(node)
+        
+        // 2 variant
+//        let firstText = SCNText(string: "Hello", extrusionDepth: 1)
+//        let textMaterial = SCNMaterial()
+//        textMaterial.diffuse.contents = UIColor.purple
+//        firstText.materials = [textMaterial]
+//        let textNode = SCNNode(geometry: firstText)
+//        textNode.position = SCNVector3(0,0,0)
+//        self.sceneView.scene.rootNode.addChildNode(textNode)
+        
+        // 3 variant
+        let text = SCNText(string: "Hello", extrusionDepth: 1)
+        text.firstMaterial?.diffuse.contents = UIColor.purple
+        text.font = UIFont(name: "Arial", size: 35)
+        let textNode = SCNNode(geometry: text)
+        textNode.position = SCNVector3(0, -1, -1)
+        self.sceneView.scene.rootNode.addChildNode(textNode)
+        
+        
+        
+//        let node = SCNNode()
+//        node.geometry = SCNPlane(width: 0.2, height: 0.2)
+//        node.geometry?.firstMaterial?.diffuse.contents = UIColor.purple
+//        node.position = SCNVector3(0,0,0)
+//        self.sceneView.scene.rootNode.addChildNode(node)
+        //node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
+        //node.geometry?.firstMaterial?.specular.contents = UIColor.orange
+        //        let x = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        //        let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        //        let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        //        node.position = SCNVector3(x,y,z)
+        
         
     }
     
@@ -33,6 +70,10 @@ class ViewController: UIViewController {
             node.removeFromParentNode()
         }
         self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
     }
     
 }
